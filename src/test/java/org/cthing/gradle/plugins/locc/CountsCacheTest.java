bdd715b832b2d290af3e713f9a8e28c4a639824b
Baron Roberts <baron@cthing.com>
2024-03-29 15:05:09 -0700

    Test coverage for the counts cache.

---
+++ src/test/java/org/cthing/gradle/plugins/locc/CountsCacheTest.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2024 C Thing Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cthing.gradle.plugins.locc;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.cthing.locc4j.Counter;
+import org.cthing.locc4j.Counts;
+import org.cthing.locc4j.Language;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CountsCacheTest {
+
+    private static final Map<Path, Map<Language, Counts>> PATH_COUNTS = new HashMap<>();
+    private static final Path CPP_PATH = Path.of("/tmp/file1.cpp");
+    private static final Path JAVA_PATH = Path.of("/tmp/file2.java");
+    private static final Path UNRECOGNIZED_PATH = Path.of("/tmp/file3.foo");
+
+    private CountsCache countsCache;
+
+    @BeforeAll
+    public static void createCounts() throws IOException {
+        final Counter counter1 = new Counter(Language.Cpp);
+        final Map<Language, Counts> counts1 =
+                counter1.count("""
+                               void App::OnMouseHook(WPARAM wParam, LPARAM lParam) {
+                                   static_cast<AppFrame*>(m_pMainWnd)->GetView()->OnMouseHook(wParam, lParam);
+                               }
+
+                               BOOL App::SaveAllModified() {
+                                   return MeaPositionLogMgr::Instance().SaveIfModified();
+                               }
+                               """);
+        final Counter counter2 = new Counter(Language.Java);
+        final Map<Language, Counts> counts2 =
+                counter2.count("""
+
+                               /**
+                                * Calculates the total line counts for all counted files.
+                                *
+                                * @return Total line count for all files and languages.
+                                */
+                               public Counts getTotalCounts() {
+                                   if (this.totalCounts == null) {
+                                       this.totalCounts = CountUtils.total(this.pathCounts);
+                                   }
+                                   return this.totalCounts;
+                               }
+
+                               """);
+        PATH_COUNTS.put(CPP_PATH, counts1);
+        PATH_COUNTS.put(JAVA_PATH, counts2);
+        PATH_COUNTS.put(UNRECOGNIZED_PATH, new EnumMap<>(Language.class));
+    }
+
+    @BeforeEach
+    public void setup() {
+        this.countsCache = new CountsCache(PATH_COUNTS);
+    }
+
+    @Test
+    public void testGetPathCounts() {
+        assertThat(this.countsCache.getPathCounts()).isEqualTo(PATH_COUNTS);
+    }
+
+    @Test
+    public void testGetLanguages() {
+        assertThat(this.countsCache.getLanguages()).containsExactlyInAnyOrder(Language.Cpp, Language.Java);
+    }
+
+    @Test
+    public void testGetTotalCounts() {
+        final Counts counts = this.countsCache.getTotalCounts();
+        assertThat(counts.getTotalLines()).isEqualTo(20);
+        assertThat(counts.getCodeLines()).isEqualTo(12);
+        assertThat(counts.getCommentLines()).isEqualTo(5);
+        assertThat(counts.getBlankLines()).isEqualTo(3);
+    }
+
+    @Test
+    public void testGetLanguageCounts() {
+        final Map<Language, Counts> counts = this.countsCache.getLanguageCounts();
+        assertThat(counts).hasSize(2);
+        assertThat(counts.get(Language.Cpp)).isEqualTo(PATH_COUNTS.get(CPP_PATH).get(Language.Cpp));
+        assertThat(counts.get(Language.Java)).isEqualTo(PATH_COUNTS.get(JAVA_PATH).get(Language.Java));
+    }
+
+    @Test
+    public void testGetFileCounts() {
+        final Map<Path, Counts> counts = this.countsCache.getFileCounts();
+        assertThat(counts).hasSize(2);
+        assertThat(counts.get(CPP_PATH)).isEqualTo(PATH_COUNTS.get(CPP_PATH).get(Language.Cpp));
+        assertThat(counts.get(JAVA_PATH)).isEqualTo(PATH_COUNTS.get(JAVA_PATH).get(Language.Java));
+    }
+
+    @Test
+    public void testGetUnrecognized() {
+        assertThat(this.countsCache.getUnrecognized()).containsExactly(UNRECOGNIZED_PATH);
+    }
+}
