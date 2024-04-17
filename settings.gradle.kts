c2f97458332bda47cfd3c1eb7bf0fa811627f3a1
Baron Roberts <baron@cthing.com>
2024-04-17 14:26:35 -0700

    Add Gradle build scan.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,15 @@ rootProject.name = "gradle-locc"
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
+    id("com.gradle.develocity") version("3.17.2")
+}
+
+develocity {
+    // Upload scans when built by GitHub actions.
+    if (System.getenv("CI") != null) {
+        buildScan {
+            termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+            termsOfUseAgree = "yes"
+        }
+    }
 }
