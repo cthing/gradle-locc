66301be1eb978090b84da9d2941fc1b97b696029
Baron Roberts <baron@cthing.com>
2024-04-18 15:09:55 -0700

    Use Kotlin build scripts for one of the integration tests to ensure API compatibility.

---
+++ src/test/resources/projects/complex-project/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("org.cthing.locc")
+}
+
+tasks {
+    countCodeLines {
+        reports {
+            xml.required = true
+            html.required = true
+            yaml.required = true
+            json.required = true
+            csv.required = true
+            text.required = true
+        }
+    }
+}
