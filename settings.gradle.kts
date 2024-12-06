94dae2816644f885d18f9a78f314d8b88eb2fffd
Baron Roberts <baron@cthing.com>
2024-12-06 15:44:42 -0800

    Upgrade Gradle and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 rootProject.name = "gradle-locc"
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
-    id("com.gradle.develocity") version("3.17.2")
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("0.9.0")
+    id("com.gradle.develocity") version("3.18.2")
 }
 
 develocity {
