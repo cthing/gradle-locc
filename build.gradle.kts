46bd156c0cafac7e0c34dd2091ab983c86758e52
Baron Roberts <baron@cthing.com>
2025-06-29 11:19:28 -0700

    Generate release candidate.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     alias(libs.plugins.versions)
 }
 
-version = ProjectVersion("2.1.0", BuildType.snapshot)
+version = ProjectVersion("2.1.0", BuildType.release)
 group = "org.cthing"
 description = "A Gradle plugin for counting lines of code."
 
